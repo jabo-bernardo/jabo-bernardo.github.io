de8b8aa2e4fad5cbaf8a6114b834262243e88c82
jabo-bernardo <joelvincent.work@gmail.com>
2024-03-30 00:06:34 +0800

    removed old portfolio

--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-const tailwindcss = require('tailwindcss');
-const isDev = process.env.NODE_ENV === 'development';
-const plugins = [ tailwindcss('./tailwind.config.js') ];
-
-module.exports = {
-	plugins: plugins
-}
